8c0c24bd1df849717f770881baefc7a8e12ba860
nzo <lorenzosarms11@gmail.com>
2018-01-29 16:42:32 +0800

    added stuff

--- app/src/main/java/com/example/ayabeltran/firstproject/MainActivity.java
+++ app/src/main/java/com/example/ayabeltran/firstproject/MainActivity.java
@@ -2,12 +2,51 @@ package com.example.ayabeltran.firstproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    dbhelper mydb;
+
+    EditText etemail, etuname, etpword, etfname, etlname;
+    Button btnreg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mydb = new dbhelper(this);
+
+        etemail = (EditText) findViewById(R.id.etEmail);
+        etuname = (EditText) findViewById(R.id.etUname);
+        etpword = (EditText) findViewById(R.id.etPword);
+        etfname = (EditText) findViewById(R.id.etFname);
+        etlname = (EditText) findViewById(R.id.etFname);
+        btnreg1 = (Button) findViewById(R.id.btnReg1);
+        btnreg2 = (Button) findViewById(R.id.btnReg2);
+
+        Register();
+    }
+
+    public void Register() {
+        btnreg2.setOnClickListener(
+                new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               boolean isInserted =  mydb.insertData(etemail.getText().toString(),
+                        etuname.getText().toString(),
+                        etpword.getText().toString(),
+                        etfname.getText().toString(),
+                        etlname.getText().toString());
+               if (isInserted = true)
+                   Toast.makeText(MainActivity.this, "you are now registered.", Toast.LENGTH_LONG).show();
+               else
+                   Toast.makeText(MainActivity.this, "your email or username is already in use.", Toast.LENGTH_LONG).show();
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/ayabeltran/firstproject/dbhelper.java
@@ -0,0 +1,54 @@
+package com.example.ayabeltran.firstproject;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Lorenzo11 on 29/01/2018.
+ */
+
+public class dbhelper extends SQLiteOpenHelper{
+
+        public static final String dbname = "task1.db";
+        public static final String Tname = "loginDetails";
+        public static final String col1 ="id";
+        public static final String col2 ="email";
+        public static final String col3 ="uname";
+        public static final String col4 ="pword";
+        public static final String col5 ="fname";
+        public static final String col6 ="lname";
+
+    public dbhelper(Context context) {
+        super(context, dbname, null, 1);
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table "+Tname+" ( "+col1+" integer primary key autoincrement, "+col2+" text, "+col3+" text, "+col4+" text, "+col5+" text, "+col6+" text, CONSTRAINT name_unique UNIQUE ("+col2+", "+col3+"));");
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table if exists "+Tname);
+        onCreate(db);
+    }
+
+    public boolean insertData(String email, String uname, String pword, String fname, String lname) {
+        SQLiteDatabase db =this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(col2, email);
+        contentValues.put(col3, uname);
+        contentValues.put(col4, pword);
+        contentValues.put(col5, fname);
+        contentValues.put(col6, lname);
+        long result = db.insert(Tname, null, contentValues);
+        if (result == -1)
+            return false;
+        else
+            return true;
+    }
+}
