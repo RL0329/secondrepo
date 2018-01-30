07b44d6d5533f8f20b947b7058327365bb07c2f6
nzo <lorenzosarms11@gmail.com>
2018-01-30 18:07:18 +0800

    img table

--- app/src/main/java/com/example/ayabeltran/firstproject/UserLogin.java
+++ app/src/main/java/com/example/ayabeltran/firstproject/UserLogin.java
@@ -2,12 +2,29 @@ package com.example.ayabeltran.firstproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class UserLogin extends AppCompatActivity {
+
+    dbhelper mydb;
+
+    EditText etloginame, etloginpword;
+    Button btn_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_login);
+        mydb = new dbhelper(this);
+
+        etloginame = findViewById(R.id.etLoginUname);
+        etloginpword = findViewById(R.id.etLogoinPword);
+        btn_login = findViewById(R.id.btn_Login);
+
+
+
+
+
     }
 }
