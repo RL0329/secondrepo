73f57d8dab4cd33b8f92c679c20aebde5c09be3c
Aya <ayabeltran14@gmail.com>
2018-02-12 09:56:01 +0800

    list  java changes

--- app/src/main/java/com/example/ayabeltran/firstproject/List.java
+++ app/src/main/java/com/example/ayabeltran/firstproject/List.java
@@ -4,6 +4,7 @@ package com.example.ayabeltran.firstproject;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.Handler;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,6 +14,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import java.util.ArrayList;
+import java.util.Random;
 
 
 public class List extends AppCompatActivity {
@@ -57,6 +59,41 @@ public class List extends AppCompatActivity {
         Log.d("Rows", cursor.getCount() + "");
 
 
+        mswipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+
+                RefreshItems();
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        mswipeRefreshLayout.setRefreshing(false);
+
+                    }
+                 },3000);
+            }
+        });
+    }
+
+        private void RefreshItems() {
+        if (cursor.moveToFirst()) {
+            do {
+                int id;
+                String name, des;
+                byte[] photo;
+
+                id = cursor.getInt(cursor.getColumnIndex("id"));
+                photo = cursor.getBlob(cursor.getColumnIndex("photo"));
+                name = cursor.getString(cursor.getColumnIndex("name"));
+                des = cursor.getString(cursor.getColumnIndex("des"));
+
+                Place places = new Place(id, photo, name, des);
+                recyclerAdapter.getPlaces().add(places);
+                recyclerAdapter.notifyDataSetChanged();
+            }
+            while (cursor.moveToNext());
+        }
+
 
 
         Add.setOnClickListener(new View.OnClickListener() {
@@ -64,59 +101,18 @@ public class List extends AppCompatActivity {
             public void onClick(View v) {
 
                 Intent toUpload = new Intent(List.this, newImg.class);
-                startActivity
-                        (toUpload);
-            }
-        });
-
-
-        mswipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                // Refresh items
-                refreshItems();
+                startActivity(toUpload);
             }
         });
     }
 
-            void refreshItems () {
-            // Load items
-                if (cursor.moveToFirst()) {
-                    do {
-                        int id;
-                        String name, des;
-                        byte[] photo;
 
-                        id = cursor.getInt(cursor.getColumnIndex("id"));
-                        photo = cursor.getBlob(cursor.getColumnIndex("photo"));
-                        name = cursor.getString(cursor.getColumnIndex("name"));
-                        des = cursor.getString(cursor.getColumnIndex("des"));
 
-                        Place places = new Place(id, photo, name, des);
-                        recyclerAdapter.getPlaces().add(places);
-                    }
-                    while (cursor.moveToNext());
-                }
 
-            // Load complete
-            onItemsLoadComplete();
-        }
-
-            void onItemsLoadComplete () {
-            // Update the adapter and notify data set changed
-                recyclerAdapter.notifyDataSetChanged();
-
-            // Stop refresh animation
-            mswipeRefreshLayout.setRefreshing(false);
-        }
 
 
 
 }
-
-
-
-
 
 
 
