0aeca3d4384e3d97b9109340bbe6a99ddfb0e973
Rean <reanmaev@gmail.com>
2018-02-26 10:05:03 +0800

    advanced endlessscroll

--- app/src/main/java/com/example/ayabeltran/firstproject/FragGrid.java
+++ app/src/main/java/com/example/ayabeltran/firstproject/FragGrid.java
@@ -64,18 +64,21 @@ public class FragGrid extends Fragment {
         sqLiteDatabase = mydb.getReadableDatabase();
         cursor = mydb.itemslisted(sqLiteDatabase);
 
-
+        refreshList();
         EndlessScroll();
         mswipeRefreshLayout.setRefreshing(false);
 
-        onLoad();
+        //onLoad();
 
 
         mswipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh(){
+                places.clear();
+                refreshList();
 
-                reload();
+                if(mswipeRefreshLayout.isRefreshing())
+                mswipeRefreshLayout.setRefreshing(false);
             }
         });
 
@@ -95,87 +98,108 @@ public class FragGrid extends Fragment {
             @Override
             public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
                 super.onScrollStateChanged(recyclerView, newState);
-                if (newState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL)
-                {
-                    isScrolling=true;
+                if (newState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL) {
+                    isScrolling = true;
                 }
             }
 
             @Override
             public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
                 super.onScrolled(recyclerView, dx, dy);
-                currentItems=mLayoutManager.getChildCount();
-                totalItems=mLayoutManager.getItemCount();
+                currentItems = mLayoutManager.getChildCount();
+                totalItems = mLayoutManager.getItemCount();
                 int rowCount = mydb.getimgTableCount();
                 pulled = pulledItens(sqLiteDatabase);
 
+                int listSize = places.size();
+                int itemsOnScreen = mLayoutManager.getChildCount();
 
-                scrollOutItems = ((GridLayoutManager)recyclerView.getLayoutManager()).findFirstVisibleItemPosition();
-                if (isScrolling && (currentItems+scrollOutItems < rowCount) && (places.size()<rowCount))
-                {
+                scrollOutItems = ((GridLayoutManager) recyclerView.getLayoutManager()).findLastVisibleItemPosition();
+
+
+                System.out.println(listSize);
+                System.out.println(scrollOutItems);
+
+
+                if (isScrolling && (scrollOutItems + 2) > places.size() && (places.size() < rowCount)) {
+
 
                     fetchData();
-                    isScrolling=false;
+//                    isScrolling = false;
+                } else {
+//                    isScrolling = false;
                 }
-                else {
-                    isScrolling=false;
-                }
+
             }
         });
     }
 
-    public Cursor pulledItens (SQLiteDatabase db) {
-        String pull = "select * from imgTable order by " + dbhelper.t2col1 + " desc limit 5 offset "+totalItems;
-        Log.d("pull", pull);
-        Cursor cursor = db.rawQuery(pull, null);
-        return cursor;
-    }
-
-    private void onLoad() {
-
-
-        if (cursor.moveToFirst()) {
-            do {
-                int id;
-                String name, des;
-                byte[] photo;
-
-                id = cursor.getInt(cursor.getColumnIndex("id"));
-                photo = cursor.getBlob(cursor.getColumnIndex("photo"));
-                name = cursor.getString(cursor.getColumnIndex("name"));
-                des = cursor.getString(cursor.getColumnIndex("des"));
-
-                Place places = new Place(id, photo, name, des);
-                gridAdapter.getPlaces().add(places);
+    public Cursor pulledItens(SQLiteDatabase db) {
+                String pull = "select * from imgTable order by " + dbhelper.t2col1 + " desc limit 5 offset " + totalItems;
+                Log.d("pull", pull);
+                Cursor cursor = db.rawQuery(pull, null);
+                return cursor;
             }
-            while (cursor.moveToNext());
-        }
-    }
-    private void reload(){
-        sqLiteDatabase.execSQL("insert into "+dbhelper.Tname2+"("+dbhelper.t2col2+","+dbhelper.t2col3+","+dbhelper.t2col4+
-                ") select "+dbhelper.t3col2+","+dbhelper.t3col3+","+dbhelper.t3col4+" from "+dbhelper.Tname3);
-
-        sqLiteDatabase.execSQL("delete from "+dbhelper.Tname3);
 
 
-        places.clear();
+    private ArrayList<Place> convertCursorToListPlace(Cursor cursor) {
+                ArrayList<Place> places = new ArrayList<>();
+                if (cursor.moveToFirst()) {
+                    do {
+                        int id;
+                        String name, des;
+                        byte[] photo;
 
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                cursor = mydb.itemslisted(sqLiteDatabase);
+                        id = cursor.getInt(cursor.getColumnIndex("id"));
+                        photo = cursor.getBlob(cursor.getColumnIndex("photo"));
+                        name = cursor.getString(cursor.getColumnIndex("name"));
+                        des = cursor.getString(cursor.getColumnIndex("des"));
 
-                gridAdapter = new GridAdapter(places,getActivity());
+                        Place place = new Place(id, photo, name, des);
+                        places.add(place);
+                    }
+                    while (cursor.moveToNext());
+
+                }
+                return places;
+            }
+    private void refreshList(){
+                Cursor cursor = getItemsFromDB();
+                populateList(cursor);
+            }
+
+    private void populateList(Cursor cursor) {
+                places = convertCursorToListPlace(FragGrid.this.cursor);
+
+                gridAdapter = new GridAdapter(places, getContext());
 
                 recyclerView.setAdapter(gridAdapter);
+            }
 
-                onLoad();
+    private Cursor getItemsFromDB(){
+                return mydb.itemslisted(sqLiteDatabase);
 
-                gridAdapter.notifyDataSetChanged();
+            }
+    private void reload() {
+                sqLiteDatabase.execSQL("insert into " + dbhelper.Tname2 + "(" + dbhelper.t2col2 + "," + dbhelper.t2col3 + "," + dbhelper.t2col4 +
+                        ") select " + dbhelper.t3col2 + "," + dbhelper.t3col3 + "," + dbhelper.t3col4 + " from " + dbhelper.Tname3);
 
-                // cancel the Visual indication of a refresh
-                mswipeRefreshLayout.setRefreshing(false);
+                sqLiteDatabase.execSQL("delete from " + dbhelper.Tname3);
+
+
+                places.clear();
+
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        //onLoad();
+
+                        gridAdapter.notifyDataSetChanged();
+
+                        // cancel the Visual indication of a refresh
+                        mswipeRefreshLayout.setRefreshing(false);
 //                getActivity().finish();
 //                startActivity(getActivity().getIntent());
 
@@ -195,43 +219,45 @@ public class FragGrid extends Fragment {
 //                ft.attach(frg);
 //                ft.commit();
 
+                    }
+                }, 3000);
             }
-        }, 3000);
-    }
-    private void fetchData(){
 
-        if (pulled.moveToFirst()) {
-            do {
-                int id;
-                String name, des;
-                byte[] photo;
+            private void fetchData() {
 
-                id = pulled.getInt(pulled.getColumnIndex("id"));
-                photo = pulled.getBlob(pulled.getColumnIndex("photo"));
-                name = pulled.getString(pulled.getColumnIndex("name"));
-                des = pulled.getString(pulled.getColumnIndex("des"));
+                if (pulled.moveToFirst()) {
+                    do {
+                        int id;
+                        String name, des;
+                        byte[] photo;
 
-                Place places = new Place(id, photo, name, des);
-                gridAdapter.getPlaces().add(places);
-            }
-            while (pulled.moveToNext());
-        }
+                        id = pulled.getInt(pulled.getColumnIndex("id"));
+                        photo = pulled.getBlob(pulled.getColumnIndex("photo"));
+                        name = pulled.getString(pulled.getColumnIndex("name"));
+                        des = pulled.getString(pulled.getColumnIndex("des"));
 
-        progressBar.setVisibility(View.VISIBLE);
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                for(int i=0;i<3;i++) {
-
-//                    gridAdapter.clearData();
-                    gridAdapter.notifyDataSetChanged();
-                    progressBar.setVisibility(View.GONE);
-
+                        Place places = new Place(id, photo, name, des);
+                        gridAdapter.getPlaces().add(places);
+                    }
+                    while (pulled.moveToNext());
                 }
 
+                progressBar.setVisibility(View.VISIBLE);
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        for (int i = 0; i < 3; i++) {
 
+//                    gridAdapter.clearData();
+                            gridAdapter.notifyDataSetChanged();
+                            progressBar.setVisibility(View.GONE);
+
+                        }
+
+
+                    }
+                }, 3000);
             }
-        }, 3000);
-    }
-}
+        }
+
